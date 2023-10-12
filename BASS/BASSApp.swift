15f9b9e14a67102a6064124b749488699dd9df6f
reembojeer <reembojeer@gmail.com>
2023-10-12 12:10:40 +0300

    Initial Commit

---
+++ BASS/BASSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BASSApp.swift
+//  BASS
+//
+//  Created by RIM ABDULLAH BUJEIR on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct BASSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
