f7469a4004ccc9a3c18d8efc47ecd5ccf4246fd2
reembojeer <reembojeer@gmail.com>
2023-10-14 07:33:32 +0300

    vn

---
+++ BASS/SwiftUIVieww.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIVieww.swift
+//  BASS
+//
+//  Created by RIM ABDULLAH BUJEIR on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIVieww: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIVieww()
+}
